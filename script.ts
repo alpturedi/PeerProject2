758b0efe336104757239e52d39e807eecfa39fd7
alpturedi <a.k.turedi@gmail.com>
2025-02-24 13:58:44 +0200

    Fix dark theme

--- script.ts
+++ script.ts
@@ -1,35 +1,25 @@
-const themeToggle = document.querySelector(
-  "#theme-toggle"
-) as HTMLButtonElement;
-const lightModeButton = document.querySelector(
-  "#theme-toggle>#light"
-) as HTMLElement;
-const darkModeButton = document.querySelector(
-  "#theme-toggle>#dark"
-) as HTMLElement;
+const themeToggle = document.querySelector("#theme-toggle") as HTMLButtonElement;
+const lightModeButton = document.querySelector("#theme-toggle>#light") as HTMLElement;
+const darkModeButton = document.querySelector("#theme-toggle>#dark") as HTMLElement;
 
-const currentTheme = localStorage.getItem("theme");
+//Initial Setup
+const initialTheme = localStorage.getItem("theme");
 
-console.log("currentTheme", currentTheme);
-
-if (currentTheme === "dark") {
+if (initialTheme === "dark") {
   document.body.classList.add("dark-mode");
 }
 
-refreshButtons(currentTheme === "dark");
+refreshButtons(initialTheme === "dark");
 
 themeToggle?.addEventListener("click", () => {
-  const temp = lightModeButton.style.display;
-  lightModeButton.style.display = darkModeButton.style.display;
-  darkModeButton.style.display = temp;
-
+  const currentTheme = localStorage.getItem("theme");
   if (currentTheme === "dark") {
     document.body.classList.remove("dark-mode");
-    console.log("LIGHT NOW");
+    refreshButtons(false);
     localStorage.removeItem("theme");
   } else {
     document.body.classList.add("dark-mode");
-    console.log("DARK NOW");
+    refreshButtons(true);
     localStorage.setItem("theme", "dark");
   }
 });
